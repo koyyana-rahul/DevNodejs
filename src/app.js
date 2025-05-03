a5f7896699ef707e90f18e73cbb6cb0dbf51ee06
unknown <koyyanarahul4002@gmail.com>
2025-05-03 14:51:44 +0530

    making database connection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 
+const connectDB = require("../src/config/database");
+
 const app = express();
 
-app.listen(7777, () => {
-  console.log("server listening on port 7777....");
-});
+connectDB()
+  .then(() => {
+    console.log("database connected");
+    app.listen(7777, () => {
+      console.log("server listening on port 7777....");
+    });
+  })
+  .catch(() => {
+    console.log("database not connected");
+  });
+
+// app.listen(7777, () => {
+//   console.log("server listening on port 7777.....");
+// });
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+     await mongoose.connect("mongodb+srv://koyyanarahul4002:kxQ9057pCAYlJ3yu@cluster0.1olsnws.mongodb.net/DEVTINDER")
+}
+
+module.exports = connectDB;
